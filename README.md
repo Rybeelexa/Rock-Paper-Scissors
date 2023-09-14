8935a39011240772cc37a855880faf24d613d853
Rybeelexa <rybeeworks18@gmail.com>
2023-09-14 10:17:02 +0300

    Made the playRound function to work. Everytime a button is clicked, it will play one round of Rock, Paper, Scissors

--- README.md
+++ README.md
@@ -2,3 +2,35 @@
 This is my first JavaScript project on The Odin Project.I hope  it's going to be fun!
  STEP 1: Create a function getComputerChoice that will randomly return either 'Rock', 'Paper' or 'Scissors'.
 So the function works when I use the OR operator, but the problem is that it only returns "Rock". There's no random outputs; which is what we want to achieve.
+STEP 2: Create a function that plays a single round of Rock, Paper,Scissors. It should also:
+1. Have two parameters(playerSelection, computerSelection)
+2. Return a string that declares the winner of the round.
+3. The playerSelection parameter should be case-insensitive. That way users can input "roCK", "ROCK" or any other variation.
+if(playerSelection==="rock" && computerSelection=== "rock") {
+    return ("DRAW! Rock balances Rock");
+} else if(playerSelection==="rock" && computerSelection === "paper") {
+    return ("YOU LOSE! Paper covers Rock!");
+} else if(playerSelection==="rock" && computerSelection==="scissors") {
+    return("YOU WIN! Rock blunts Scissors");
+}  else if(playerSelection==="paper" && computerSelection==="rock") {
+    return("YOU WIN! Paper covers Rock");
+}  else if(playerSelection==="paper" && computerSelection==="paper") {
+    return("DRAW! Paper evens out paper");
+}  else if(playerSelection==="paper" && computerSelection==="scissors") {
+    return("YOU LOSE! Scissors slices paper");
+}  else if(playerSelection==="scissors" && computerSelection==="rock") {
+    return("YOU LOSE! Rock blunts Scissors");
+}  else if(playerSelection==="scissors" && computerSelection==="paper") {
+    return("YOU WIN! Scissors slices Paper");
+}  else if(playerSelection==="scissors" && computerSelection==="scissors") {
+    return("DRAW! Scissors evens out Scissors");
+} else {
+    return("Choose your weapon wisely!");
+}
+Rock("&#9994" or "&#x270A")
+Rock("&#9995" or "&#x270B")
+Rock("&#9996" or "&#x270C")
+const myArray = ["rock", "paper", "scissors"];
+function getComputerChoice() {
+    return myArray[~~(Math.random() * myArray.length)];
+}
--- script.js
+++ script.js
@@ -1,7 +1,49 @@
-const myArray = ["Rock", "Paper", "Scissors"];
+let playerSelection;
+let computerSelection;
+
+const myArray = ["rock", "paper", "scissors"];
 function getComputerChoice() {
-    return [~~(Math.random() * myArray.length)];
+    return myArray[~~(Math.random() * myArray.length)];
 }
+computerSelection = getComputerChoice();
+//alert(computerSelection);
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === "rock" && computerSelection === "rock") {
+        alert("DRAW! Rock balances Rock");
+    } else if(playerSelection === "rock" && computerSelection === "paper") {
+        alert("YOU LOSE! Paper covers Rock!");
+    } else if(playerSelection === "rock" && computerSelection === "scissors") {
+        alert("YOU WIN! Rock blunts Scissors");
+    }  else if(playerSelection === "paper" && computerSelection === "rock") {
+        alert("YOU WIN! Paper covers Rock");
+    }  else if(playerSelection === "paper" && computerSelection === "paper") {
+        alert("DRAW! Paper evens out paper");
+    }  else if(playerSelection === "paper" && computerSelection === "scissors") {
+        alert("YOU LOSE! Scissors slices paper");
+    }  else if(playerSelection === "scissors" && computerSelection === "rock") {
+        alert("YOU LOSE! Rock blunts Scissors");
+    }  else if(playerSelection === "scissors" && computerSelection === "paper") {
+        alert("YOU WIN! Scissors slices Paper");
+    }  else if(playerSelection === "scissors" && computerSelection === "scissors") {
+        alert("DRAW! Scissors evens out Scissors");
+    } else {
+        alert("Choose your weapon wisely!");
+    }
+}
+
+const selectionButtons = document.querySelectorAll('[data-selection]');
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+       const selectionName = selectionButton.dataset.selection;
+       playerSelection = selectionName.toLowerCase();
+       playRound(playerSelection, computerSelection);
+       //alert(playerSelection);
+    });
+});
+
+
+
 
 
 
